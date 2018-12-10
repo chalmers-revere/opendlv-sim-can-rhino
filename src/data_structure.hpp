7cede23b5533d62f086ebbae5aac70bc604c073f
Adam2092 <adam.2092@gmail.com>
2018-12-10 10:08:02 +0100

    Added copyright clarification comments

--- src/data_structure.hpp
+++ src/data_structure.hpp
@@ -1,8 +1,23 @@
-/** This header file contains necessary data structures
-*   for code transplant from Matlab to C++
-*
-*   Yue Kang and Yushu Yu, 
-*   Revere Lab, Chalmers / GU, 2018
+/*
+ * Copyright (C) 2018 Yushu Yu and Yue Kang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This header file contains necessary data structures
+ * for code transplant from Matlab to C++
+ *
+ * Yue Kang and Yushu Yu, 
+ * Revere Lab, Chalmers / GU, 2018
 */
 
 #include <iostream>
